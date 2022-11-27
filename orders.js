2343bd1d82062b0412db58a0586e93aeb30aff6b
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-11-28 01:04:27 +0400

    insert data in table

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Fodable Mini Drone",
+    productNumber: "8989",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Larvender MKFGJ Drone",
+    productNumber: "9889",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Ruko MJH& Drone",
+    productNumber: "34589",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with camere  Drone",
+    productNumber: "7889",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Fodable Mini Drone",
+    productNumber: "8989",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with camere  Drone",
+    productNumber: "7889",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Larvender MKFGJ Drone",
+    productNumber: "9889",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+];
